117ac45ca763cb11c73e664b7263e8217f1ba880
Mary McMahan <mary@fanexperiences.com>
2016-06-04 14:10:31 -0500

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'Np98Nh2L3vaNs22tRZAET9FFW',
+  consumer_secret: 'avbO2nqxSd7AmGvLgElp97kbMLJpCEXCU1WpPRafKeHv4N5DII',
+  access_token_key: '3956432475-nsDIhbiugIINaLQHQyAxndoXoDB4GBxQXl0RWZ1',
+  access_token_secret: 't2MLCdrsHIvO2SI9gOPReYGmerc1atgLKC0VQpWdSDgBq',
 }
